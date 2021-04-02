d50094617e8f626bd7677005ebfcfa236ef229a7
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-04-03 02:48:10 +0800

    support create demo directory on fly

--- zalias.zsh
+++ zalias.zsh
@@ -6,30 +6,59 @@ alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
 alias code="code-insiders"
 
-alias ngxstart="nginx"
-alias ngxreload="nginx -s reload"
-alias ngxstop="nginx -s stop"
-alias ngxtest="nginx -t"
-alias ngxconfig="code -f '/usr/local/etc/nginx/servers/glorystudio.conf'"
+alias nginxstart="nginx"
+alias nginxreload="nginx -s reload"
+alias nginxstop="nginx -s stop"
+alias nginxtest="nginx -t"
+alias nginxconfig="code -r '/usr/local/etc/nginx/servers/glorystudio.conf'"
+
+alias hosts="code -r '/etc/hosts'"
+
+alias zshrcconfig="code -r ~/.zshrc"
+alias zaliasconfig="code -r $ZSH_CUSTOM/zalias.zsh"
+alias srczshrc="source ~/.zshrc"
+
+alias mongostart="brew services start mongodb-community"
+alias mongorestart="brew services restart mongodb-community"
+alias mongostop="brew services stop mongodb-community"
 
 opendemo() {
-  local demoname;
-  local dir;
-  local alldemos=();
+  local demosdir=$demos; # variable 'demos' is a global shortcut refered to demos directory
+  local demoname; # name of selected or created demo
+  local demodir; # directory of selected or created demo
+  local alldemos=(); # array type, all demos in 'demos' directory
 
-  # Get all the folder names of $demos directory,
+  # Create a demo directory
+  createDemoDir() {
+    mkdir -p $demosdir/$1;
+    print -P "%F{green}Directory $demosdir/$1 was successfully created.%f";
+    if read -qs -t 30 "?>> Do you want to open '$1'? [Y/n]"$'\n'; then
+      code -r "$demosdir/$1";
+    fi
+  }
+
+  if ( test -n $1 ) && ( test "$1" = "-m" ) && ( test -n $2 ); then
+    createDemoDir $2;
+    return;
+  fi
+
+  # Get all the demo names in $demosdir,
   # Push into alldemos
-  local oldPath=`pwd`;
-  cd $demos;
-  local count=1;
-  for f in *
-  do
-    alldemos[$count]="$f";
-    count=`expr $count + 1`;
-  done
-  cd $oldPath;
+  updateAllDemos() {
+    local currentDir=`pwd`;
+    cd $demosdir;
+    local count=1;
+    for f in *
+    do
+      alldemos[$count]="$f";
+      count=`expr $count + 1`;
+    done
+    # changed back to current working directory
+    cd $currentDir;
+  }
 
   listAndRead() {
+    updateAllDemos;
     local count=0;
     print "================================";
     for f in ${alldemos[@]}
@@ -50,8 +79,8 @@ opendemo() {
       demoname=$1;
     fi
 
-    dir="$demos/$demoname";
-    if test $demoname && ( test -d $dir )
+    demodir="$demosdir/$demoname";
+    if test $demoname && ( test -d $demodir )
     then
       dirExists=true;
     else
@@ -59,7 +88,7 @@ opendemo() {
     fi
   }
   
-  # Input argument or not, which points to its demo folder name straightforward
+  # Input argument or not, which refer to its demo folder name straightforward
   if [ -z $1 ]
   then
     listAndRead;
@@ -71,11 +100,21 @@ opendemo() {
   check $demoname;
   while [ "$dirExists" = false ]
   do
-    print -P "%F{red}$dir is not a demo directory. Try again.%f";
+    print -P "%F{red}$demodir is not a demo directory.%f";
+    if read -qs -t 30 "?>> Do you want to create demo '$demoname'? [Y/n]"$'\n'; then
+      if [ -n $demoname ]; then
+        createDemoDir $demoname;
+        return;
+      else
+        print -P "%F{red}Invalid directory name%f";
+      fi
+    fi
     listAndRead;
     check $demoname;
   done
 
-  code -r $dir;
-  print -P "%F{green}$dir successfully open.%f";
+  code -r $demodir;
+  print -P "%F{green}$demodir successfully open.%f";
 }
+
+transfer(){ if [ $# -eq 0 ];then echo "No arguments specified.\nUsage:\n transfer <file|directory>\n ... | transfer <file_name>">&2;return 1;fi;if tty -s;then file="$1";file_name=$(basename "$file");if [ ! -e "$file" ];then echo "$file: No such file or directory">&2;return 1;fi;if [ -d "$file" ];then file_name="$file_name.zip" ,;(cd "$file"&&zip -r -q - .)|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null,;else cat "$file"|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;else file_name=$1;curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;}
